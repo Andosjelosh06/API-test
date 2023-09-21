33606df77ae827939c218b47b1d6a6e1ce265893
Anders Stanger <ansta032@osloskolen.no>
2023-09-21 12:28:27 +0200

    intial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+randomJokeBtn.addEventListener("mouseout", function () {
+    this.textContent = "Vis Tilfeldig Vits";
+    this.classList.remove("font-change");
+});
+
+
+function klikk() {
+    // Generer en unik tidslig verdi
+    var timestamp = new Date().getTime();
+    // Oppdater bilde
+    var randomImage = document.getElementById("randomImage");
+    randomImage.src = "https://loremflickr.com/320/240/paris,girl/all?" + timestamp;
+}
+
+document.getElementById("randomJokeBtn").addEventListener("click", function () {
+    // Utfør en HTTP GET-request for å hente tilfeldig vits fra APIet
+    fetch("https://api.chucknorris.io/jokes/random")
+        .then(response => response.json())
+        .then(data => {
+            // Oppdater teksten med den tilfeldige vitsen
+            document.getElementById("jokeText").textContent = data.value;
+        })
+        .catch(error => {
+            console.error("Feil ved henting av vits:", error);
+        });
+});
